04858ac3d0897fdca499e5dd987c5b265367d886
Sebastian Bille <sebastianbille@outlook.com>
2022-01-17 12:20:15 +0100

    fix: broken multi actions same line & service

--- src/completionProvider.ts
+++ src/completionProvider.ts
@@ -12,24 +12,26 @@ export const getCompletionItemProvider = (services: Record<string, IamService>):
     console.debug('inside actions array, adding suggestions');
 
     const fullWordRange = document.getWordRangeAtPosition(position, /[a-z0-9:-]+/i);
-    const [serviceWord] = document.getText(fullWordRange).split(':');
+    const fullWord = fullWordRange && document.getText(fullWordRange);
+    if (fullWord) {
+      const [serviceWord] = fullWord.split(':');
 
-    const service = serviceWord && services[serviceWord];
-    if (service) {
-      const lineText = document.lineAt(position.line).text;
-      const actionOffset = lineText.indexOf(`${service.servicePrefix}:`) + service.servicePrefix.length + 1;
-      const suggestions: CompletionItem[] = service.actions.map(action => ({
-        label: action.name,
-        kind: CompletionItemKind.Field,
-        data: action,
-        documentation: createActionDocs(action),
-        // Set explicit range as default behavior is to replace the whole current word.
-        // that causes the service prefix to be overwritten with the action so we instead
-        // set a range that starts from the current position instead
-        range: new Range(position.line, actionOffset, position.line, position.character),
-      }));
+      const service = serviceWord && services[serviceWord];
+      if (service) {
+        const actionOffset = fullWord.indexOf(':') >= 0 ? fullWord.indexOf(':') + 1 + fullWordRange?.start.character : 0;
+        const suggestions: CompletionItem[] = service.actions.map(action => ({
+          label: action.name,
+          kind: CompletionItemKind.Field,
+          data: action,
+          documentation: createActionDocs(action),
+          // Set explicit range as default behavior is to replace the whole current word.
+          // that causes the service prefix to be overwritten with the action so we instead
+          // set a range that starts from the current position instead
+          range: new Range(position.line, actionOffset, position.line, position.character),
+        }));
 
-      return { items: suggestions, isIncomplete: true };
+        return { items: suggestions, isIncomplete: true };
+      }
     }
 
     const partialWordRange = document.getWordRangeAtPosition(position);
