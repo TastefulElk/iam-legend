f736e9bc85c917f067cf68eb9d508ac62c461552
Sebastian Bille <sebastianbille@outlook.com>
2022-01-17 10:28:55 +0100

    build: add test workflow

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -74,7 +74,7 @@ suite("CompletionProvider", () => {
 
 const triggerAndAcceptSuggestion = async () => {
   await commands.executeCommand('editor.action.triggerSuggest');
-  await delay(100); // wait for the suggestions to be populated
+  await delay(500); // wait for the suggestions to be populated
   await commands.executeCommand('acceptSelectedSuggestion');
 };
 
