b5438b0bccde635f7723fb245929f66afc0439dd
Sebastian Bille <sebastianbille@outlook.com>
2022-01-28 01:00:48 +0100

    refactor(completionProvider): start restrucure code

--- src/completionProvider.ts
+++ src/completionProvider.ts
@@ -3,6 +3,55 @@ import { createActionDocs, createServiceDocs } from "./documentation";
 import { IamService, IamServicesByPrefix } from "./iamActions";
 import { isInsideActionsArray } from "./utility/utility";
 
+const getActionSuggestions = (services: IamService[], range: Range) => {
+  const suggestions: CompletionItem[] = services.map(x => x.actions.map(action => ({
+    label: action.name,
+    kind: CompletionItemKind.Field,
+    data: action,
+    documentation: createActionDocs(action),
+    detail: x.serviceName,
+    // Set explicit range as default behavior is to replace the whole current word.
+    // that causes the service prefix to be overwritten with the action so we instead
+    // set a range that starts from the current position instead
+    range
+  }))).flat();
+
+  return { items: suggestions, isIncomplete: true };
+};
+
+const getServiceSuggestions = (matchedWord: string, servicesByPrefix: IamServicesByPrefix, range: Range) => {
+  // if the matched word starts or ends with quotes, help the json language server
+  // to find the correct service name by applying it to the suggestions as it otherwise
+  // fails to suggest it
+  const labelPrefix = matchedWord.startsWith('"')
+    ? '"' : matchedWord.startsWith(`'`)
+      ? `'` : '';
+
+  const labelSuffix = matchedWord.endsWith('"')
+    ? '"' : matchedWord.endsWith(`'`)
+      ? `'` : '';
+  
+  const suggestions: CompletionItem[] = Object.values(servicesByPrefix).map(service => service.map(x => ({
+    label: `${labelPrefix}${x.servicePrefix}`,
+    filterText: `${labelPrefix}${x.servicePrefix}${labelSuffix}`,
+    kind: CompletionItemKind.Module,
+    documentation: createServiceDocs(x),
+    detail: x.serviceName,
+    // Set explicit range as default behavior is to replace the whole current word.
+    // that causes the service prefix to overwrite the action if already present
+    // set a range that starts from the current position instead
+    range
+  }))).flat();
+
+  // preselect the first suggestion, since we likely have the most relevant suggestion here
+  suggestions[0].preselect = true;
+
+  return { items: suggestions, isIncomplete: true };
+};
+
+const getActionOffset = (word: string, range: Range) => word.indexOf(':') >= 0 ? word.indexOf(':') + 1 + range?.start.character : 0;
+const getActionRange = (position: Position, actionOffset: number) => new Range(position.line, actionOffset, position.line, position.character);
+
 export const getCompletionItemProvider = (servicesByPrefix: IamServicesByPrefix): CompletionItemProvider => ({
   provideCompletionItems(document: TextDocument, position: Position) {
     if (!isInsideActionsArray(document, position)) {
@@ -16,49 +65,16 @@ export const getCompletionItemProvider = (servicesByPrefix: IamServicesByPrefix)
 
       const services = serviceWord && servicesByPrefix[serviceWord];
       if (services) {
-        const actionOffset = fullWord.indexOf(':') >= 0 ? fullWord.indexOf(':') + 1 + fullWordRange?.start.character : 0;
-        const suggestions: CompletionItem[] = services.map(x => x.actions.map(action => ({
-          label: action.name,
-          kind: CompletionItemKind.Field,
-          data: action,
-          documentation: createActionDocs(action),
-          // Set explicit range as default behavior is to replace the whole current word.
-          // that causes the service prefix to be overwritten with the action so we instead
-          // set a range that starts from the current position instead
-          detail: x.serviceName,
-          range: new Range(position.line, actionOffset, position.line, position.character),
-        }))).flat();
-
-        return { items: suggestions, isIncomplete: true };
+        const actionOffset = getActionOffset(fullWord, fullWordRange);
+        const actionRange = getActionRange(position, actionOffset);
+        return getActionSuggestions(services, actionRange);
       }
     }
 
     const partialWordRange = document.getWordRangeAtPosition(position);
-    const word = partialWordRange ? document.getText(partialWordRange) : '';
+    const matchedWord = partialWordRange ? document.getText(partialWordRange) : '';
 
-    const labelPrefix = word.startsWith('"')
-      ? '"' : word.startsWith(`'`)
-        ? `'` : '';
-
-    const labelSuffix = word.endsWith('"')
-      ? '"' : word.endsWith(`'`)
-        ? `'` : '';
-
-    const suggestions: CompletionItem[] = Object.values(servicesByPrefix).map(service => service.map(x => ({
-      label: `${labelPrefix}${x.servicePrefix}`,
-      filterText: `${labelPrefix}${x.servicePrefix}${labelSuffix}`,
-      kind: CompletionItemKind.Module,
-      documentation: createServiceDocs(x),
-      detail: x.serviceName,
-      // Set explicit range as default behavior is to replace the whole current word.
-      // that causes the service prefix to overwrite the action if already present
-      // set a range that starts from the current position instead
-      range: new Range(position.line, partialWordRange?.start.character as number || position.character, position.line, position.character),
-    }))).flat();
-
-    // preselect the first suggestion, since we likely have the most relevant suggestion here
-    suggestions[0].preselect = true;
-
-    return { items: suggestions, isIncomplete: true };
+    const serviceRange = new Range(position.line, partialWordRange?.start.character as number || position.character, position.line, position.character);
+    return getServiceSuggestions(matchedWord,servicesByPrefix, serviceRange);
   }
 });
